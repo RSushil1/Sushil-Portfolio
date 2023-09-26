9e903a4f12a72612f2ef44c73d37f6d132da7199
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-09-26 13:51:24 +0530

    ..

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {fileURLToPath}= require('url');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('./client'))
+app.use(express.static('./client/index.html'))
 
 dotenv.config();
 
